d8f602bad29d2c9f30682edaa8b6497f44b9e7ab
Benedikt Koop <benedikteliaskoop@gmail.com>
2026-05-12 13:04:42 +0200

    Implement Encode
    -renamed OneHotEncode to Encode to better represent what it does (both categorical and numerical encoding)
    -implemented a categorical one hot encoding
    -implemented numerical features sum, mean, min, max, std, count
    -converted feature matrix into a sparse matrix, and output caseIds series and outcome label series.

--- src/featureEngineering/oneHotFE.py
+++ src/featureEngineering/oneHotFE.py
@@ -7,31 +7,60 @@ from scipy.sparse import csr_matrix
 Feature engineering step to encode the dataframe into a feature matrix. For the sake of the tracer bullet being created in sprint 1, this version will be implemented through a variation of one hot encoding.
 WHAT THIS VERSION CAN DO:
 simplify each trace into a single row
-assert whether an activity is contained in the trace
-assert simple metrics to each row (trace length, trace lifecycle etc.)
+assert whether any categorical trait is contained in the trace
+assert for each numerical trait certain values (sum, mean, min, max, std, count)
 
 WHAT THIS VERSION CANT DO:
 recognise duplicate activities
 recognise activity ordering
 recognise looping structures / causalities
-
+temporal features left out for the tracer bullet, since the outcome definition is wholly based on time.
 
 
 INPUT: as input, a dataframe is taken with an extra 'outcome' column. A boolean input for prefix generation will be added, but the prefix generation itself not implemented for this tracer further than a 
 docking spot in the code. 
 
-OUTPUT: Sparse matrix, and a time series, as a tuple. 
+OUTPUT: Sparse matrix, outcome label series, and caseId series, as a tuple. 
 
 
 '''
 
 
-def oneHotEncode(df: pd.DataFrame, prefix: bool) -> tuple[csr_matrix, pd.Series]:
-    
+def Encode(df: pd.DataFrame, prefix: bool) -> tuple[csr_matrix, pd.Series, pd.Series]:
     
 
-    if(prefix):
-        #missing prefix generation
-        raise NotImplementedError("prefix generation left out for sprint 1 tracer bullet")
+
+    
+    #Validate for id, activity, outcome
+    required = {"case:concept:name", "concept:name", "outcome"}
+    missing = required - set(df.columns)
+
+    if missing:
+        raise ValueError(f"Missing required columns: {missing}") 
+
+
+    #OH encode each categorical column
+    featureMatrix = pd.DataFrame(index=df["case:concept:name"].unique())
+    for col in df.columns:
+        if col in {"case:concept:name", "outcome", "time:timestamp"}:
+            continue
+        if (pd.api.types.is_string_dtype(df[col]) or pd.api.types.is_categorical_dtype(df[col])):
+            temp = pd.crosstab(df["case:concept:name"],df[col]).clip(upper=1)
+            temp = temp.add_prefix(f"{col}=")
+
+        elif pd.api.types.is_numeric_dtype(df[col]):
+            temp = df.groupby("case:concept:name")[col].agg(["sum", "mean", "min", "max", "std","count"])
+            temp = temp.add_prefix(f"{col}_")
+        
+        featureMatrix = featureMatrix.join(temp)
+    
+    y = df.groupby("case:concept:name")["outcome"].first() ##Outcome as time series
+    case_ids = featureMatrix.index
+    X_sparse = csr_matrix(featureMatrix.fillna(0).values)
+
+    return X_sparse,y,case_ids
+
+    
+    
     
 
